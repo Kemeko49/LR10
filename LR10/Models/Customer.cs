39c4d2366d471b3e416b939f79e617e99cf90370
Artem Ovcharenko <artemovcharenko30@gmail.com>
2024-04-08 23:12:02 +0300

    Добавьте файлы проекта.

---
+++ LR10/Models/Customer.cs
@@ -0,0 +1,64 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LR10.Models
+{
+    public class Customer
+    {
+        [Required(ErrorMessage = "Ім'я та прізвище обов'язкові!")]
+        public string? Initials { get; set; }
+
+        [Required(ErrorMessage = "Для заповнення потрібна електронна адреса!")]
+        [EmailAddress(ErrorMessage = "Недійсна електронна адреса!")]
+        public string? Email { get; set; }
+
+        [Required(ErrorMessage = "Бажана дата консультації обов'язкова!")]
+        [FutureDate(ErrorMessage = "Бажана дата консультації має бути в майбутньому!")]
+        [NonWeekendDay(ErrorMessage = "Бажана дата консультації не може бути вихідним днем!")]
+        public DateTime? ConsultationDate { get; set; }
+
+        [Required(ErrorMessage = "Виберіть тему для консультації!")]
+        [ProductNotAllowedOnMonday(ErrorMessage = "Тема не може бути «Основи», якщо дата консультації - понеділок!")]
+        public string? Product { get; set; }
+
+        public override string ToString()
+        {
+            return $"Initials: {Initials}.\nEmail: {Email}.\nConsultation date: {ConsultationDate}.\nChosen product: {Product}";
+        }
+    }
+
+    public class FutureDateAttribute : ValidationAttribute
+    {
+        public override bool IsValid(object value)
+        {
+            DateTime dt = (DateTime)value;
+            return dt.Date > DateTime.Now.Date;
+        }
+    }
+
+    public class NonWeekendDayAttribute : ValidationAttribute
+    {
+        public override bool IsValid(object value)
+        {
+            DateTime dt = (DateTime)value;
+            return dt.DayOfWeek != DayOfWeek.Saturday && dt.DayOfWeek != DayOfWeek.Sunday;
+        }
+    }
+
+    public class ProductNotAllowedOnMondayAttribute : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            var customer = (Customer)validationContext.ObjectInstance;
+
+            if (customer.ConsultationDate.HasValue && customer.ConsultationDate.Value.DayOfWeek == DayOfWeek.Monday)
+            {
+                if (value != null && value.ToString().Equals("Основи", StringComparison.OrdinalIgnoreCase))
+                {
+                    return new ValidationResult("Тема не може бути «Основи», якщо дата консультації - понеділок");
+                }
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+}
